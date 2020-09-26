05b06f2eb6d2de72da210fd9cd42df102b7bad30
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2020-09-25 22:33:56 -0500

    still need min and max

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,34 +2,33 @@
 import os
 #reading CSV File
 import csv
-csvpath=os.path.join("C:\\Users\\coset\\OneDrive\\Desktop\\python-challenge\\PyBank\\Resources\\Budget_data.csv.csv")
+csvpath=os.path.join("Resources\Budget_data.csv.csv")
 with open(csvpath,'r') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",")
-        header= next(csvreader)
-        for x in csvreader:
-                filename= 'Resources\Budget_data.csv.csv'
-                months = int(float(filename[0]) + 1
-                print(months)
-                total = int(float(filename[1])
-                #return [total]
-                #Analyze total Number of months
-                #Months = int(float(1 for line in open(filename))-1)
-                MonthNum = sum(months)
-        #return [monthNum]
-                print (f"Financial Analysis----------------Total Months: {int(MonthNum)}")
-        #Analyze net total amount of profit/losses
-                Total_Profit = total + 0
-#Average of changes in profit losses
-                Average_Profits= Total_Profit /monthNum
-#greatest increase in profits Date and Amount
-#greatest decrease in losses  date and amount
-#print to terminal
-        print(f"Financial Analysis----------------Total Months:{int(MonthNum)}")
-        print(f"Total:+ {int(Total_Profit)}")
-        print(f"Average Change: +{str(Average_Profits)}")
-        print(f"Greatest INcrease in Profits:{str(Great_Month)}")
-        print(f"Greatest Decrease in Profits:{str(Least_Month)}")
+        header = next(csvreader)
 
-#export to txtfile
-with open(output_file, "w",newline='') as datafile:
-        datafile.write(Output)
+        profits = []
+        months = []
+
+        for row in csvreader:
+                profit = int(row[1])
+                profits.append(profit)
+                month = str(row[0])  
+                months.append(month)
+
+                total_months = len(profits)
+                total_profit = sum(profits)
+                month_change = [profits[i + 1] - profits[i] for i in range(len(profits)-1)]
+                change_count = len(month_change)
+                summonth=sum(month_change)
+                total_change =round(summonth/(total_months))
+                summonth=sum(month_change)
+                great_inc = max(month_change)
+                great_dec = min(month_change)
+        
+print(f"Financial Analysis \n --------------")
+print(f"Total Months:{total_months}")
+print(f"Total: ${total_profit}")
+print(f"Average Change: ${total_change}")
+print(f"Greatest Increase in profits: {great_inc}")
+print(f"Greatest Decrease in profits: {great_dec}")
